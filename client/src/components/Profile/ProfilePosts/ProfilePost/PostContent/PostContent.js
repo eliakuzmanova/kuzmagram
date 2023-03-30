cb96751bedd347184ed42044fdef6fdbf7aa934a
Elia Kuzmanova <eliamitova7@gmail.com>
2023-03-30 11:46:31 +0200

    add likes

--- client/src/components/Profile/ProfilePosts/ProfilePost/PostContent/PostContent.js
+++ client/src/components/Profile/ProfilePosts/ProfilePost/PostContent/PostContent.js
@@ -6,14 +6,55 @@ import { HiOutlineBookmark } from "react-icons/hi2";
 import { HiOutlineHeart } from "react-icons/hi2";
 import { HiOutlineChatBubbleOvalLeft } from "react-icons/hi2";
 import { HiOutlineXMark } from "react-icons/hi2";
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ProfileContext } from "../../../../../contexts/ProfileContext"
+import * as postService from "../../../../../services/postService"
+import { useAuthContext } from '../../../../..//contexts/AuthContext';
 
 export default function PostContent({
     onModalClose,
     post
 }) {
+
+    // add logic to stay liked or unlike thhe btn <-------------
+    const { userId } = useAuthContext()
     const user = useContext(ProfileContext)
+    const [heartClicked, setHeartClicked] = useState(false)
+    const [fetchedPost, setFetchedPost] = useState("")
+    const [postLikes, setPostLikes] = useState(post.likes)
+    useEffect(() => {
+        fetchPost()
+        console.log("Hello from fetch");
+    }, [])
+   
+    async function fetchPost() {
+        const result = await postService.getOne(post._id)
+            setFetchedPost(result)
+            setPostLikes(result.likes)
+            if (postLikes.includes(userId)) {
+                setHeartClicked(true)
+            } else {
+                setHeartClicked(false)
+            }
+            console.log("Hello from fetch func");
+    }
+
+    async function onLike() {
+        let updatedPost;
+        if (!heartClicked) {
+
+            updatedPost = await postService.likePost(fetchedPost._id, userId)
+            setPostLikes(state => ([...state, userId]))
+            setHeartClicked(true)
+        } else {
+
+            updatedPost = await postService.dislikePost(fetchedPost._id, userId)
+
+            setPostLikes(state => state.filter(state => state != userId))
+            setHeartClicked(false)
+        }
+ 
+    }
     return (
         <div className={styles["modal"]}>
             <HiOutlineXMark className={styles["close-btn"]} onClick={onModalClose} />
@@ -27,35 +68,33 @@ export default function PostContent({
                     <section className={styles["post-information-side"]}>
                         <section className={styles["user-section"]}>
                             <div className={styles["user-container"]}>
-                                <img className={styles["user-photo"]} src={user.image? `http://localhost:7070/${user.image}` :require("../../../../../images/user-profile-image.png")} alt="" />
+                                <img className={styles["user-photo"]} src={user.image ? `http://localhost:7070/${user.image}` : require("../../../../../images/user-profile-image.png")} alt="" />
                                 <Link className={styles["username"]}>{user.username}</Link>
                             </div>
                             <HiEllipsisHorizontal className={styles["user-section-icon"]} />
                         </section>
                         <div className={styles["description-container"]}>
                             <div className={styles["description-user-image-container"]}>
-                            <img className={styles["description-user-image"]} src={user.image? `http://localhost:7070/${user.image}` :require("../../../../../images/user-profile-image.png")} alt="user" />
+                                <img className={styles["description-user-image"]} src={user.image ? `http://localhost:7070/${user.image}` : require("../../../../../images/user-profile-image.png")} alt="user" />
                             </div>
                             {post.description && <p className={styles["description"]}> <Link className={styles["username-description"]}>{user.username}</Link> {post.description} </p>}
                         </div>
-                            <ModalComments post={post}/>
-                        
+                        <ModalComments post={post} />
+
 
                         <section className={styles["icons-section"]}>
                             <div className={styles["actions-container"]}>
 
-                                <HiOutlineHeart className={styles["icon"]} />
+                                <HiOutlineHeart className={heartClicked ? styles["icon-heart-clicked"] : styles["icon-heart"]} onClick={onLike} />
 
-                                <HiOutlineChatBubbleOvalLeft className={`${styles["icon"]} ${styles["icon-message"]}`} />
+                                <HiOutlineChatBubbleOvalLeft className={styles["icon-message"]} />
 
                             </div>
-                            <div className={styles["last-icon-container"]}>
-                                <HiOutlineBookmark className={styles["icon"]} />
-                            </div>
+
                         </section>
                         <section className={styles["comments-area-section"]}>
                             <div className={styles["text-likes-container"]}>
-                                <p className={styles["text-likes"]}>Some <b>people</b> like that</p>
+                                <p className={styles["text-likes"]}><b>{postLikes.length}</b> people like that</p>
                             </div>
 
                             <div className={styles["textarea-container"]}>
