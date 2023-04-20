ef5b69a6acf7607de1d3dfd141256b2893a23f06
Elia Kuzmanova <eliamitova7@gmail.com>
2023-04-20 21:16:58 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,6 @@ app.use("/uploads", express.static("uploads"))
 app.use(router)
 
 mongoose.set({"strictQuery": false})
-mongoose.connect("mongodb+srv://eliakuzmanova7:Amollbabyboo.7@cluster0.x2agojk.mongodb.net/Kuzmagram")
+mongoose.connect("mongodb+srv://eliakuzmanova7:VnkSWL7nGKEwMbuQ@cluster0.o0cltbg.mongodb.net/kuzmagram")
 
 app.listen(7070, console.log("Server is listening on port 7070..."))
